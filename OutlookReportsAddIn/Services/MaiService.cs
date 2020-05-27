45619f7d7567a6c7694b010a8c604cb68b0df689
kaanlab <kaan@live.ru>
2020-05-27 17:26:38 +0300

    Add ability to filter for mail address

--- OutlookReportsAddIn/Services/MaiService.cs
+++ OutlookReportsAddIn/Services/MaiService.cs
@@ -3,99 +3,50 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
-
+using OutlookReportsAddIn.Extentions;
 
 namespace OutlookReportsAddIn.Services
 {
     public class MailService
     {
-        public IEnumerable<Mail> InboxMails((DateTime startDate, DateTime endDate) datesRange)
-        {
-            return SearchInFolder(datesRange, OlDefaultFolders.olFolderInbox);
-
-        }
-        public IEnumerable<Mail> OutboxMails((DateTime startDate, DateTime endDate) datesRange)
-        {
-            return SearchInFolder(datesRange, OlDefaultFolders.olFolderOutbox);
-
-        }
-
-        private IEnumerable<Mail> SearchInFolder((DateTime startDate, DateTime endDate) datesRange, OlDefaultFolders olFolder)
+        public IEnumerable<Mail> SearchMails((DateTime startDate, DateTime endDate) datesRange)
         {
             var emailItems = new List<Mail>();
+            emailItems.AddRange(SearchIn(datesRange, OlDefaultFolders.olFolderInbox));
+            emailItems.AddRange(SearchIn(datesRange, OlDefaultFolders.olFolderOutbox));
+            emailItems.Sort((x, y) => DateTime.Compare(x.Date, y.Date));
 
-            var accounts = Globals.ThisAddIn.Application.Session.Accounts;
+            return emailItems;
+        }
 
+        private IEnumerable<Mail> SearchIn((DateTime startDate, DateTime endDate) datesRange, OlDefaultFolders olFolder)
+        {
+            var emailItems = new List<Mail>();
+            var accounts = Globals.ThisAddIn.Application.ActiveExplorer().Session.Accounts;
 
             foreach (Account acc in accounts)
             {
-                if (acc.AccountType == OlAccountType.olPop3)
-                {
-                    var stores = Globals.ThisAddIn.Application.Session.Stores;
-                    foreach (Store store in stores)
-                    {
+                var stores = acc.Session.Stores;
 
-                        var inboxFolder = store.GetDefaultFolder(olFolder);
-                        var filtredItiems = inboxFolder.Items.
-                            Restrict("[ReceivedTime] >= '" + datesRange.startDate.ToString("g") + "' And [ReceivedTime] <= '" + datesRange.endDate.ToString("g") + "'");
+                foreach (Store store in stores)
+                {
+                    var folder = store.GetDefaultFolder(olFolder);
+                    if (folder.FolderPath.Contains(Properties.Settings.Default.MailAddress))
+                    {
+                        var filtredItiems = folder.Items.FiltredByDates(datesRange);
 
                         foreach (var item in filtredItiems)
                         {
                             if (item is MailItem)
                             {
-                                var emailItem = new Mail();
-
-                                // sender
-                                emailItem.SenderAddress = ((MailItem)item).SenderEmailAddress;
-
-                                // attachments
-                                if (((MailItem)item).Attachments.Count > 0)
-                                {
-                                    var sb = new StringBuilder();
-
-                                    for (int i = 1; i <= ((MailItem)item).Attachments.Count; i++)
-                                    {
-                                        sb.Append(((MailItem)item).Attachments[i].FileName + " (" + ((MailItem)item).Attachments[i].Size / 1000 + " КБ); \n");
-                                        emailItem.Attachments = sb.ToString();
-                                    }
-                                }
-                                else
-                                {
-                                    emailItem.Attachments = " - ";
-                                }
-
-                                //category
-                                switch (((MailItem)item).Importance)
-                                {
-                                    case OlImportance.olImportanceNormal:
-                                        emailItem.Category = "Обычная";
-                                        break;
-                                    case OlImportance.olImportanceHigh:
-                                        emailItem.Category = "Срочно";
-                                        break;
-                                    default:
-                                        emailItem.Category = "Низкая";
-                                        break;
-                                }
-
-                                // date
-                                emailItem.Date = ((MailItem)item).ReceivedTime;
-
-                                // to
-                                emailItem.RecivedAddress = ((MailItem)item).To;
-
-                                // description
-                                emailItem.Subject = "Тема сообщения: " + ((MailItem)item).Subject;
-
-                                emailItems.Add(emailItem);
+                                var olMail = ((MailItem)item);
+                                emailItems.Add(olMail.MapToMail());
                             }
                         }
                     }
                 }
             }
-
             return emailItems;
-
         }
     }
 }
