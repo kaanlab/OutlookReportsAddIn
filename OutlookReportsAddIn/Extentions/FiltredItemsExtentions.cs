33e141550b17806fb72d09df25d5c6849e967f07
kaanlab <kaan@live.ru>
2020-05-27 17:23:40 +0300

    Add Externtions methods

---
+++ OutlookReportsAddIn/Extentions/FiltredItemsExtentions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Office.Interop.Outlook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutlookReportsAddIn.Extentions
+{
+    public static class FiltredItemsExtentions
+    {
+        public static Items FiltredByDates(this Items items, (DateTime startDate, DateTime endDate) datesRange)
+        {
+            return items.Restrict("[ReceivedTime] >= '" + datesRange.startDate.ToString("g") + "' And [ReceivedTime] <= '" + datesRange.endDate.ToString("g") + "'");
+        }
+    }
+}
---
+++ OutlookReportsAddIn/Extentions/ModelMappingExtentions.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Office.Interop.Outlook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutlookReportsAddIn.Extentions
+{
+    public static class ModelMappingExtentions
+    {
+        public static Mail MapToMail(this MailItem outlookMail)
+        {
+            var emailItem = new Mail();
+
+            // sender
+            emailItem.SenderAddress = outlookMail.SenderEmailAddress;
+
+            // attachments
+            if (outlookMail.Attachments.Count > 0)
+            {
+                var sb = new StringBuilder();
+
+                for (int i = 1; i <= outlookMail.Attachments.Count; i++)
+                {
+                    sb.Append(outlookMail.Attachments[i].FileName + " (" + outlookMail.Attachments[i].Size / 1000 + " КБ); \n");
+                    emailItem.Attachments = sb.ToString();
+                }
+            }
+            else
+            {
+                emailItem.Attachments = " - ";
+            }
+
+            //category
+            switch (outlookMail.Importance)
+            {
+                case OlImportance.olImportanceNormal:
+                    emailItem.Category = "Обычная";
+                    break;
+                case OlImportance.olImportanceHigh:
+                    emailItem.Category = "Срочно";
+                    break;
+                default:
+                    emailItem.Category = "Низкая";
+                    break;
+            }
+
+            // date
+            emailItem.Date = outlookMail.ReceivedTime;
+
+            // to
+            emailItem.RecivedAddress = outlookMail.To;
+
+            // description
+            emailItem.Subject = "Тема сообщения: " + outlookMail.Subject;
+
+            return emailItem;
+        }
+    }
+}
